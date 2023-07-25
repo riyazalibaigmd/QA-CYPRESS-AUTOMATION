b417d96d60d6b0f485f8b043ab5a78dd163ca6cd
Riyaz Ali Baig Mohammad <riyazalibaig.mohammad@cesltd.com>
2023-07-25 16:49:31 +0530

    Added Mochawesome Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Cypress_Automation_Reports",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    ignoreVideos: false,
+    "timestamp":"mmddyyyy_HHMMss"
+  },
   e2e: {
     baseUrl: "https://demoqa.com/",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
-  },
-  defaultCommandTimeout: 4000,
-  pageLoadTimeout: 30000,
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true,
   },
 });
